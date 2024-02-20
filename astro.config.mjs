0504e159b742b276943320308622cb7464ce705d
Oleg M <013gmatsk@gmail.com>
2024-02-21 00:47:39 +0300

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-
+import preact from "@astrojs/preact";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: 'eu', // optional,  or 'eu' (default)
-      },
-    })
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      // Add your components here
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: 'eu' // optional,  or 'eu' (default)
+    }
+  }), preact()]
 });
